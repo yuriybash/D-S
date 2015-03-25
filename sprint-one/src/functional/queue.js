c4c32424b2772558ebf3f878c9decd7b8f2b5485
henrymollman <hmollman@gmail.com>
2015-03-25 14:33:24 -0700

    initial commit

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,30 @@ var Queue = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {},
+      size = 0,
+      queue = 0,
+      value;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    value = storage[queue];
+    if (size > 0) {
+      size --;
+    }
+    if (queue < size) queue++;
+    else queue = size;
+    return value;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,27 @@ var Stack = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {},
+      size = 0,
+      value;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
-  someInstance.pop = function(){
+  someInstance.pop = function() {
+    
+    value = storage[size - 1];
+    if (size > 0) {
+      size--;
+    };
+    return value;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
