dccb47e4d958c251cc89b5d4f22e03e089b046a0
yuriybash <yuriybash@gmail.com>
2015-03-25 20:04:49 -0700

    Finished through pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.num = 0;
+  this.order = 0;
+
 };
 
+Queue.prototype.size = function() {
+  return this.num;
+};
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.num + this.order] = value;
+  this.num++;
+}
+
+Queue.prototype.dequeue = function() {
+  var temp = this.storage[this.order];
+  if (this.num > 0) {
+    this.num--;
+  }
+  if (this.order < this.num) {
+    this.order++;
+  }
+  return temp;
+}
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.num = 0;
+  this.storage = {};
 };
 
+Stack.prototype.size = function() {
+  return this.num;
+}
 
+Stack.prototype.push = function(value) {
+  this.storage[this.num] = value;
+  this.num++;
+};
+
+Stack.prototype.pop = function() {
+  var temp = this.storage[this.num - 1];
+  if (this.num > 0) {
+    this.num--;
+  };
+  return temp;
+}
